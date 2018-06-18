44cf52d4606a23660aa511c05842e6ebed01058e
Umyippa <sywhitehouse@gmail.com>
2018-06-18 13:30:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { PinkFloyd: "The Wall"}
 
-function updatePlaylist(playlist, 'artistName', 'songTitle') {
+function updatePlaylist(playlist, "", "") {
   return Object.assign({}, playlist, { [artistName]: songTitle})
 }
