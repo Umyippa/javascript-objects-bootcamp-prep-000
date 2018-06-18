64b54834a0554c8b6aaa18078c896bd66092de0b
Umyippa <sywhitehouse@gmail.com>
2018-06-18 15:07:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName: "songTitle"}
 
 function updatePlaylist(playlist, [artistName], [songTitle]) {
-  playlist.Phil Ochs = "Here's to the State of Mississippi";
+  playlist.PhilOchs = "Here's to the State of Mississippi" ;
   
 }
