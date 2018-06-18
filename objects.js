cd03535d437e970bc0c5ce2b3dfdade95df64841
Umyippa <sywhitehouse@gmail.com>
2018-06-18 13:15:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = { PinkFloyd: "The Wall"}
 
 function updatePlayList(playlist, artisName, songTitle) {
-  var updatePlayList = 
+  var updatePlayList = {
     artisName: 'PinkFloyd';
     songTitle: 'The Wall';
-  
+  }
     return playlist;
 }
