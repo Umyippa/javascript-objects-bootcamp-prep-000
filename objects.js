a033cf0391e097755e4f8a9f9399482f313ac010
Umyippa <sywhitehouse@gmail.com>
2018-06-18 13:04:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { PinkFloyd: "The Wall"}
 
 function updatePlayList(playlist, PinkFloyd, TheWall) {
-  playlist[PinkFloyd] = "The Wall";
+  playlist['PinkFloyd'] = 'The Wall';
   
   return playlist;
 }
