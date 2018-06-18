ba7e410e0cfcdd8a16adec7092048d6e4e42386a
Umyippa <sywhitehouse@gmail.com>
2018-06-18 13:00:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { PinkFloyd: "The Wall"}
 
-function updatePlayList(playlist, PinkFloyd, "The Wall") {
+function updatePlayList(playlist, PinkFloyd, The Wall) {
   playlist[PinkFloyd] = "The Wall";
   
   return playlist
